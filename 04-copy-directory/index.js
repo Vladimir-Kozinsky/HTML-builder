490892ec44e8a1e154a83a9c86467f1cd87eaaed
Vladimir <kozinskytxc@gmail.com>
2022-05-17 14:20:00 +0400

    feat: add 04-copy-directory task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const path = require('path');
+const { readdir, copyFile, mkdir, rm } = require('fs/promises');
 
+const folderPath = path.join(__dirname, 'files');
+const copyFolderPath = path.join(__dirname, 'copyFiles');
+
+async function copyDir() {
+    let files = await readdir(folderPath);
+    mkdir(copyFolderPath, { recursive: true });
+    for (const file of files) {
+        let destination = path.join(copyFolderPath, file);
+        let filePath = path.join(folderPath, file)
+        await copyFile(filePath, destination);
+    }
+    update(files)
+}
+
+async function update(files) {
+    let copyFiles = await readdir(copyFolderPath);
+    for (const file of copyFiles) {
+        let isFind = files.find(item => item === file);
+        if (!isFind) {
+            let delFilePath = path.join(copyFolderPath, file);
+            rm(delFilePath);
+        }
+    }
+}
+
+copyDir();
