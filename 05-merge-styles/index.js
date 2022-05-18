cbf866c0ab2d45ffe3e6f58766a42a9baffdaf6b
Vladimir <kozinskytxc@gmail.com>
2022-05-18 12:38:24 +0400

    feat: add 05-merge-styles task solution

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+const { readdir, copyFile, mkdir, rm } = require('fs/promises');
+
+const folderPath = path.join(__dirname, 'styles');
+const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
+
+async function bundle() {
+    let files = await readdir(folderPath);
+    clearFile();
+    for (const file of files) {
+        const filePath = path.join(folderPath, file);
+        const ext = path.extname(filePath);
+        if (ext === '.css') {
+            fs.readFile(filePath, 'utf-8', (err, data) => {
+                fs.appendFile(bundlePath, data + '\n', err => {
+                    if (err) throw err;
+                });
+            })
+        }
+    }
+}
+
+
+function clearFile() {
+    fs.truncate(bundlePath, err => {
+        if (err) throw err; // не удалось очистить файл
+    });
+}
+
+bundle();
