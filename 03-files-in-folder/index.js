3f96e633306cb713c25e57eb98f53ae6b007adcf
Vladimir Kozinsky <kozinskytxc@gmail.com>
2022-05-20 21:16:24 +0400

    fix: 03-files-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,26 +5,7 @@ const folderPath = path.join(__dirname, '/secret-folder');
 
 // FUNCTION WRITE FILES FROM SECRET FOLDER ONLY 
 
-// (async () => {
-//     try {
-//         const files = await readdir(folderPath, { withFileTypes: true });
-//         for (const file of files) {
-//             if (file.isFile()) {
-//                 let filePath = path.join(folderPath, file.name)
-//                 stat(filePath, (err, stats) => {
-//                     let nameParts = file.name.split('.');
-//                     console.log(`${nameParts[0]} - ${nameParts[1]} - ${stats.size/1000}kb`)
-//                 });
-//             }
-//         }
-//     } catch (err) {
-//         console.error(err);
-//     }
-// })();
-
-// FUNCTION WRITE ALL FILES
-
-async function filesFinder(folderPath) {
+(async () => {
     try {
         const files = await readdir(folderPath, { withFileTypes: true });
         for (const file of files) {
@@ -32,16 +13,35 @@ async function filesFinder(folderPath) {
                 let filePath = path.join(folderPath, file.name)
                 stat(filePath, (err, stats) => {
                     let nameParts = file.name.split('.');
-                    console.log(`${nameParts[0]} - ${nameParts[1]} - ${stats.size / 1000}kb`)
+                    console.log(`${nameParts[0]} - ${nameParts[1]} - ${stats.size/1000}kb`)
                 });
-            } else {
-                let newDir = path.join(__dirname, '/secret-folder', file.name);
-                filesFinder(newDir);
             }
         }
     } catch (err) {
         console.error(err);
     }
-}
+})();
 
-filesFinder(folderPath);
+// FUNCTION WRITE ALL FILES
+
+// async function filesFinder(folderPath) {
+//     try {
+//         const files = await readdir(folderPath, { withFileTypes: true });
+//         for (const file of files) {
+//             if (file.isFile()) {
+//                 let filePath = path.join(folderPath, file.name)
+//                 stat(filePath, (err, stats) => {
+//                     let nameParts = file.name.split('.');
+//                     console.log(`${nameParts[0]} - ${nameParts[1]} - ${stats.size / 1000}kb`)
+//                 });
+//             } else {
+//                 let newDir = path.join(__dirname, '/secret-folder', file.name);
+//                 filesFinder(newDir);
+//             }
+//         }
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
+
+// filesFinder(folderPath);
