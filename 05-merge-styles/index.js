64667c2637410ca789d8747885869018d705c112
Vladimir <kozinskytxc@gmail.com>
2022-05-20 13:39:08 +0400

    fix: 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,29 +3,31 @@ const fs = require('fs');
 const { readdir } = require('fs/promises');
 
 const folderPath = path.join(__dirname, 'styles');
+const bundleFolderPath = path.join(__dirname, 'project-dist');
 const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
 
-async function bundle() {
+// function clearFile() {
+//     fs.truncate(bundlePath, err => {
+//         if (err) throw err; // не удалось очистить файл
+//     });
+// }
+
+const output = fs.createWriteStream(bundlePath);
+(async () => {
+    // const bundleFiles = await readdir(bundleFolderPath);
+    // const isBundle = bundleFiles.find(file => file === 'bundle.css');
+    // console.log(isBundle);
+    // if (isBundle) {
+    //   // clearFile();
+    // }
     let files = await readdir(folderPath);
-    clearFile();
     for (const file of files) {
         const filePath = path.join(folderPath, file);
         const ext = path.extname(filePath);
         if (ext === '.css') {
-            fs.readFile(filePath, 'utf-8', (err, data) => {
-                fs.appendFile(bundlePath, data + '\n', err => {
-                    if (err) throw err;
-                });
-            })
+            const input = fs.createReadStream(filePath, 'utf-8');
+            input.pipe(output);
+            input.on('error', error => console.log('Error', error.message));
         }
     }
-}
-
-
-function clearFile() {
-    fs.truncate(bundlePath, err => {
-        if (err) throw err; // не удалось очистить файл
-    });
-}
-
-bundle();
+})();
