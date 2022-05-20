0f493fb69f932d67fd7ef90ec6fc71a639247a31
Vladimir <kozinskytxc@gmail.com>
2022-05-20 13:38:26 +0400

    fix: 02-write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,6 +6,7 @@ const { stdin: input, stdout: output } = require('process');
 const rl = readline.createInterface({ input, output });
 const filePath = path.join(__dirname, 'text.txt');
 const outputStream = fs.createWriteStream(filePath);
+
 rl.question('Please enter any text:', (data) => {
     outputStream.write(data);
     console.log("Enter something else:");
@@ -27,21 +28,4 @@ rl.on('SIGINT', () => {
 }
 );
 
-
-
-
-
-// const filePath = path.join(__dirname, 'text.txt');
-// const output = fs.createWriteStream(filePath);
-
-// stdin.on('data', data => {
-//     const dataStr = data.toString();
-//     console.log(dataStr);
-//     if (dataStr == 'exit') {
-
-//         process.exit();
-//     } else {
-//         output.write(data)
-//     }
-// });
 
