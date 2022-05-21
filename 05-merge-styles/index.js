178e0924139b5efea0caf96f42f4c04b59a2df2e
Vladimir <kozinskytxc@gmail.com>
2022-05-21 11:27:51 +0400

    fix: 05-merge-styles task del comments

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,23 +3,10 @@ const fs = require('fs');
 const { readdir } = require('fs/promises');
 
 const folderPath = path.join(__dirname, 'styles');
-const bundleFolderPath = path.join(__dirname, 'project-dist');
 const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
-
-// function clearFile() {
-//     fs.truncate(bundlePath, err => {
-//         if (err) throw err; // не удалось очистить файл
-//     });
-// }
 
 const output = fs.createWriteStream(bundlePath);
 (async () => {
-    // const bundleFiles = await readdir(bundleFolderPath);
-    // const isBundle = bundleFiles.find(file => file === 'bundle.css');
-    // console.log(isBundle);
-    // if (isBundle) {
-    //   // clearFile();
-    // }
     let files = await readdir(folderPath);
     for (const file of files) {
         const filePath = path.join(folderPath, file);
